2a207fa0db105600a4c109c7b1258d8c1f9a53fc
JAIME ISAAC LÓPEZ GUERRERO <0322103746@ut-tijuana.edu.mx>
2025-06-05 20:14:42 -0700

    Avance Final

--- core/forms.py
+++ core/forms.py
@@ -13,3 +13,16 @@ class CreateBankForm(forms.ModelForm):
             "bank_name": forms.TextInput(attrs={"type": "text", "class": "from-control", "placeholder": "Escribe el nombre del banco"}),
             "address": forms.TextInput(attrs={"type": "text", "class": "from-control", "placeholder": "Escribe la dirección del banco"}),
         }
+
+class UpdateBankForm(forms.ModelForm):
+    class Meta:
+        model = models.Bank
+        fields = [
+            "bank_name",
+            "address",
+            "status",
+        ]
+        widgets = {
+            "bank_name": forms.TextInput(attrs={"type": "text", "class": "from-control", "placeholder": "Escribe el nombre del banco"}),
+            "address": forms.TextInput(attrs={"type": "text", "class": "from-control", "placeholder": "Escribe la dirección del banco"}),
+        }
